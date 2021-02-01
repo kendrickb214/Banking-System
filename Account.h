f53c8a29eb35aa7780757f002fb963fcd1f0a746
kendrickb214 <75757550+kendrickb214@users.noreply.github.com>
2021-01-31 18:21:46 -0600

    Update Account.h

--- Account.h
+++ Account.h
@@ -8,17 +8,23 @@
 #include <iostream>
 #include <string>
 #include "I_Printable.h"
-#include <vector>
+
 
 class Account:  public I_Printable {
 
 public:
+    private:
+    static constexpr const char *def_name = "Unnamed Account";
+    static constexpr double def_balance = 0.0;
+protected:
+    std::string name;
+    double balance;
+public:
+    Account(std::string name = def_name, double balance = def_balance);
     virtual bool deposit(double amount) = 0;
     virtual bool withdraw(double amount) = 0;
     virtual void print(std::ostream &os) const = 0;
     virtual ~Account(){}
-
-
 };
 
 #endif //NEW_ACCOUNT_H
